1293e8873441d14746d1001fb26a75f489ac338b
Nguyễn Hoàng Sơn <sonnhhe186586@fpt.edu.vn>
2024-01-17 09:05:15 +0700

    Add modal and screen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from '../login/login.component';
+import { StudentComponent } from '../student/student.component';
 
 export const routes: Routes = [
-    { path: 'login', component: LoginComponent }
+    { path: 'login', component: LoginComponent },
+    { path: 'student', component: StudentComponent   }
 ];
--- src/login/login.component.ts
+++ src/login/login.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [NgIf,ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
-export class LoginComponent {
-  title = 'Login account';
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  submitted = false;
+  loading = false;
+  returnUrl!: string;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+  }
+  get f() {
+    return this.loginForm.controls;
+  }
+  onSubmit(event:Event) {
+    event.preventDefault();
+    //console.log(this.f['username'].value);
+    this.submitted = true;
+    setTimeout(() => {
+      this.router.navigate(['../student'], {queryParams:null});
+    
+    }, 3000);
+    // stop here if form is invalid
+
+    this.loading = true;
+  }
 }
