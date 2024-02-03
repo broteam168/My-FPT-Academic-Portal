a7bf39098a274c80e2e5dda191ce3a0e5ad97bc1
Nguyễn Hoàng Sơn <sonnhhe186586@fpt.edu.vn>
2024-02-03 13:51:28 +0700

    add model and service submajor

---
+++ src/Models/submajor.ts
@@ -0,0 +1,10 @@
+export class Major {
+    id: number;
+    description: string;
+    name: string;
+    fullName: string;
+    category: string;
+    degreeLevel: string;
+    isActive: boolean;
+  }
+  
--- src/Services/index.ts
+++ src/Services/index.ts
@@ -3,3 +3,4 @@ export * from "./auth.service";
 export * from "./school.service";
 export * from "./class.service";
 export * from "./major.service";
+export * from "./submajor.service";
